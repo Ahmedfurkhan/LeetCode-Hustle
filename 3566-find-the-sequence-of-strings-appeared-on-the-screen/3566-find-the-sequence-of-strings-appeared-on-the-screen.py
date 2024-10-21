4e3cb634955862ec81a725c1e2ab3e177c8e17d3
Ahmed Furkhan <108826853+Ahmedfurkhan@users.noreply.github.com>
2024-10-21 20:31:54 +0530

    Time: 27 ms (100%), Space: 25.9 MB (100%) - LeetHub

--- 3566-find-the-sequence-of-strings-appeared-on-the-screen/3566-find-the-sequence-of-strings-appeared-on-the-screen.py
+++ 3566-find-the-sequence-of-strings-appeared-on-the-screen/3566-find-the-sequence-of-strings-appeared-on-the-screen.py
@@ -1,19 +1,31 @@
 class Solution(object):
     def stringSequence(self, target):
         """
+        This function generates a sequence of strings that eventually builds up to the target string.
+        The sequence is formed by starting with 'a' and adding characters to the end of the current string.
+        If the current string is the same as the target string at a particular position, 'a' is added to the end of the current string.
+        Otherwise, the character in the current string is incremented.
+
         :type target: str
         :rtype: List[str]
         """
-        ans=['a']
-        i=0
-        j=0
-        while ans[-1]!=target:
-            if ans[-1][j]==target[i]:
-                ans.append(ans[-1][:j+1]+'a')
-                j+=1
-                i+=1
-            else:
-                ans.append(ans[-1][:j]+chr(ord(ans[-1][j])+1))
-        return ans
 
-        
+        # Initialize the answer list with 'a', and two pointers for the current string and the target string
+        ans = ['a']
+        i = 0  # pointer for the target string
+        j = 0  # pointer for the current string
+
+        # Continue generating the sequence until the target string is reached
+        while ans[-1] != target:
+            # Check if the current character in the current string is the same as the current character in the target string
+            if ans[-1][j] == target[i]:
+                # If they are the same, append a new string to the sequence by adding 'a' to the end of the current string
+                ans.append(ans[-1][:j+1] + 'a')
+                j += 1  # move the pointer for the current string forward
+                i += 1  # move the pointer for the target string forward
+            else:
+                # If they are different, append a new string to the sequence by incrementing the character in the current string
+                ans.append(ans[-1][:j] + chr(ord(ans[-1][j])+1))
+
+        # Return the generated sequence of strings
+        return ans
