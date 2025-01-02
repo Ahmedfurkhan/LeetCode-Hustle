f0a3b5835760725544b7fce22dc04380b7ab0234
Ahmed Furkhan <108826853+Ahmedfurkhan@users.noreply.github.com>
2025-01-03 00:00:35 +0530

    Time: 15 ms (93.12%), Space: 49.6 MB (47.74%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.py
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.py
@@ -1,24 +1,16 @@
 class Solution:
-    def vowelStrings(
-        self, words: List[str], queries: List[List[int]]
-    ) -> List[int]:
-        ans = [0] * len(queries)
-        vowels = {"a", "e", "i", "o", "u"}
-        prefix_sum = [0] * len(words)
-        sum = 0
-        for i in range(len(words)):
-            current_word = words[i]
-            if (
-                current_word[0] in vowels
-                and current_word[len(current_word) - 1] in vowels
-            ):
-                sum += 1
-            prefix_sum[i] = sum
+    def vowelStrings(self, words: List[str], queries: List[List[int]]) -> List[int]:
+        n = len(words)
+        Prefix = [0] * (n + 1)
+        vowels = {'a', 'e', 'i', 'o', 'u'}
 
-        for i in range(len(queries)):
-            current_query = queries[i]
-            ans[i] = prefix_sum[current_query[1]] - (
-                0 if current_query[0] == 0 else prefix_sum[current_query[0] - 1]
-            )
+        for i in range(n):
+            Prefix[i + 1] = Prefix[i]
+            if words[i][0] in vowels and words[i][-1] in vowels:
+                Prefix[i + 1] += 1
 
-        return ans
+        ANS = []
+        for query in queries:
+            ANS.append(Prefix[query[1] + 1] - Prefix[query[0]])
+
+        return ANS
